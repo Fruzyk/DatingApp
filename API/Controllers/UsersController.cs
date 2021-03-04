045bb1030188386ddedec1dfa614a65a895b19ce
fruzyk <patryk.fruzynski@gmail.com>
2021-03-04 19:25:32 +0100

    Adding authentication

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,14 +3,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    
+    public class UsersController : BaseAPIController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -20,11 +20,12 @@ namespace API.Controllers
 
         // api/users/
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
-
+        [Authorize]
         // api/users/id
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUsers(int id)
